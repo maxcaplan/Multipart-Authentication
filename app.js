c3275e186a11e00f289f881e190cbf4f76740305
Jellycream <maxacaplan@gmail.com>
2019-06-05 14:39:20 -0300

    image post requsts added

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
 const app = express()
 const port = process.env.npm_package_config_myPort || 8080
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+
+
+app.post('/api/photo', function (req, res) {
+    if (req.body.data == false) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    var string = req.body.data
+    var regex = /^data:.+\/(.+);base64,(.*)$/;
+
+    var matches = string.match(regex);
+    var ext = matches[1];
+    var data = matches[2];
+    var buffer = Buffer.from(data, 'base64');
+    fs.writeFileSync('testImages/' + Date.now() + '.' + ext, buffer);
+
+    res.send("done")
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`))   
